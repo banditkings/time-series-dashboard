5b50d9cf0eb8040d7dbfbac58706f90951e44051
Nelson Tang <nelson.tang17@gmail.com>
2022-06-07 13:32:35 -0700

    added line plot, datatable, download button

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # time-series-dashboard
-A simple 1-page dashboard example using Dash &amp; Python to create a user forecasting interface
+A simple 1-page dashboard example using Dash & Python to create a user forecasting interface
 
 ## Key Features
 
-* [ ] Line plot that plots historical data, forecast, and upper/lower prediction intervlas
-* [ ] Editable Datatable with the time series forecast that can be overridden
+* [x] Line plot that plots historical data, forecast, and upper/lower prediction intervals
+* [x] Editable Datatable with the time series forecast that can be overridden
 * [ ] Abilty to upload own dataset (1000 row limit) as CSV and use that for historicals
-* [ ] Ability to download forecast as CSV
+* [x] Ability to download forecast as CSV
+* [ ] Add styling and modularize
+
+## How to run
+
+```bash
+python dashboard.py
+```
+
+## Screenshot
+
+![img](docs/screenshot.png)
--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,15 @@ from statsmodels.tsa.exponential_smoothing.ets import ETSModel
 
 df = pd.read_csv('https://raw.githubusercontent.com/banditkings/datasets/main/TimeSeries/AirPassengers.csv')
 
-app = Dash(__name__)
+external_stylesheets = [
+     "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.css.config.serve_locally = True
+
+models = ["NaiveForeaster", "AutoETS", "StatsForecastAutoARIMA", 
+          "ThetaForecaster", "TBATS"]
 
 def forecast(df):
     model = ETSModel(df["#Passengers"],
@@ -23,30 +31,39 @@ def forecast(df):
     result = pred.summary_frame(alpha=0.05)
     result['Month'] = ['1961-01', '1961-02', '1961-03', '1961-04', '1961-05', '1961-06']
     result.rename({'mean':'Forecast'}, axis=1, inplace=True)
+    result = result[['Month', 'Forecast', 'pi_lower', 'pi_upper']].round(2)
     return result
 
-forecast = forecast(df)
+fcst = forecast(df)
 
 app.layout = html.Div([
+    # Title
+    html.H1(children="Dashboard Title", id="db-name"),
+    # Line Chart with Historicals and Forecast
     dcc.Graph(id='table-editing-simple-output'),
+    # Editable DataTable where user can override the forecast
     dash_table.DataTable(
         id='table-editing-simple',
         columns=(
-            [{"name": i, "id": i} for i in forecast.columns]
+            [{"name": i, "id": i} for i in fcst.columns]
         ),
-        data=forecast.to_dict('records'),
+        data=fcst.to_dict('records'),
         editable=True,
         fixed_rows={'headers': True},  # Freeze Header
         page_size=100,
+        export_format='csv',
+        style_cell={'textAlign': 'center', 'font_family':"'Press Start 2P'"},
         style_table={'height': '300px', 'overflowY': 'auto'}, # Vertical Scrollbar
     ),
-])
+], id="main_page")
 
+# Create a callback to update the figure based on what's in the datatable
 @app.callback(
     Output('table-editing-simple-output', 'figure'),
     Input('table-editing-simple', 'data'),
     Input('table-editing-simple', 'columns'))
 def display_output(rows, columns):
+    """Update the figure based on the datatable contents"""
     fcst_df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df.tail(50).Month, y=df.tail(50)['#Passengers'], name="Actuals"))
@@ -57,7 +74,8 @@ def display_output(rows, columns):
     fig.add_trace(go.Scatter(x=fcst_df.Month, y=fcst_df['pi_lower'], 
                              name='Lower PI', mode='lines',
                              line={'color':'grey', 'dash':'dot'}))
+    fig.update_layout(font_family="'Press Start 2P', Arial")
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='localhost')
+    app.run_server(debug=True, host='localhost', port=1234)
